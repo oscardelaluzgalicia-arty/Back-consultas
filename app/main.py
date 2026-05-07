464d92f115048e0afd2eabaa4756c8c60d0991a6
Francisco de la luz <oscardelaluzgalicia@gmail.com>
2026-05-06 19:42:42 -0600

    arcihvos vacios

--- app/main.py
+++ app/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from api.verify import router as verify_router
 from api.tables import router as tables_router
 from api.describe import router as describe_router
-from api.rows import router as rows_router
 from api.query import router as query_router
 
 app = FastAPI()
 app.include_router(verify_router)
 app.include_router(tables_router)
 app.include_router(describe_router)
-app.include_router(rows_router)
 app.include_router(query_router)
